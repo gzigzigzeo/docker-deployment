9cea3061958851b2815ac6f91ddc9287f4c1ee08
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-01-29 22:31:59 +0400

    Other test

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,11 +6,11 @@ main() {
 	docker build -t gcr.io/mobius-network/hello-app:${REVISION} .
 	gcloud docker -- push gcr.io/mobius-network/hello-app:${REVISION}
 
-	sed 's/__REVISION__/'"$REVISION"'/g' < manifests/helloweb-deployment.yaml > manifests/helloweb-deployment.yaml
+	cat manifests/helloweb-deployment.yaml | sed 's/__REVISION__/'"$REVISION"'/g' > manifests/helloweb-deployment-r.yaml
 
-	cat manifests/helloweb-deployment.yaml
+	cat manifests/helloweb-deployment-r.yaml
 
-	kubectl apply -f manifests/helloweb-deployment.yaml
+	kubectl apply -f manifests/helloweb-deployment-r.yaml
 	kubectl apply -f manifests/helloweb-service.yaml
 
 	kubectl get pods
