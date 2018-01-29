5b2744e32f7eb488554862a9b76fa7534e334dcd
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-01-29 22:18:23 +0400

    REVISION bash hack

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,9 +3,10 @@
 set -euo pipefail
 
 main() {
-	gcloud docker -- pull gcr.io/mobius-network/hello-app:latest
-	docker build -t gcr.io/mobius-network/hello-app:latest .
-	gcloud docker -- push gcr.io/mobius-network/hello-app:latest
+	docker build -t gcr.io/mobius-network/hello-app:${REVISION} .
+	gcloud docker -- push gcr.io/mobius-network/hello-app:${REVISION}
+
+	sed 's/__REVISION__/'"$REVISION"'/g' < manifests/helloweb-deployment.yaml > manifests/helloweb-deployment.yaml
 
 	kubectl apply -f manifests/helloweb-deployment.yaml
 	kubectl apply -f manifests/helloweb-service.yaml
